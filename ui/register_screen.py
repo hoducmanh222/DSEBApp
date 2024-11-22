2ec40d297f6cb07cca33553faddca1863d342a05
davizd <153365142+hoducmanh222@users.noreply.github.com>
2024-11-22 15:36:08 +0700

    Update register_screen.py

--- ui/register_screen.py
+++ ui/register_screen.py
@@ -1,17 +1,47 @@
 # register_screen.py
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 import json
 import os
 from hashlib import sha256
+from kivy.properties import StringProperty
 
 class RegisterScreen(MDScreen):
+    logo_path = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(RegisterScreen, self).__init__(**kwargs)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.logo_path = os.path.join(current_dir, '..', 'photo', 'neulogo.jpg')
+        self.dialog = None
+
+    def show_error_dialog(self, message):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Error",
+                text=message,
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        on_release=lambda x: self.dialog.dismiss()
+                    )
+                ]
+            )
+        else:
+            self.dialog.text = message
+        self.dialog.open()
     def register(self):
         try:
             username = self.ids.username_field.text.strip()
             password = self.ids.password_field.text.strip()
             
             if not username or not password:
-                self.ids.error_label.text = "Please fill all fields"
+                self.show_error_dialog("Username and password are required.")
+                return
+            
+            if  len(password) < 6:
+                self.show_error_dialog("Password must be at least 6 characters long.")
                 return
 
             # Get absolute path to data directory
@@ -32,10 +62,11 @@ class RegisterScreen(MDScreen):
 
             # Add new user
             hashed_password = sha256(password.encode()).hexdigest()
-            data['users'].append({
-                'username': username,
-                'password': hashed_password
-            })
+            if username not in [user['username'] for user in data['users']]:
+                data['users'].append({'username': username, 'password': hashed_password})
+            else:
+                self.show_error_dialog("Username already exists.")
+                return
 
             # Save data
             with open(data_file, 'w') as f:
